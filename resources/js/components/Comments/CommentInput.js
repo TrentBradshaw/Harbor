c22fd7f117e32b348093f6c81dd53fc41698b875
TrentBradshaw <TrentHarrisonBradshaw@gmail.com>
2021-02-11 23:43:42 -0800

    linting

--- resources/js/components/Comments/CommentInput.js
+++ resources/js/components/Comments/CommentInput.js
@@ -1,69 +1,53 @@
-
 import React, { useState } from 'react';
 
-//don't forget to pass the votes into the voting system component
+// don't forget to pass the votes into the voting system component
 
-    function CommentInput({isReply, parentComment, appendNewComment, parentPostId, hideInputChange}) {
-        const [text, updateText] = useState('')
-        return (
-            <div>
-                <div style={{ marginLeft: '5%', marginRight: '11%', minHeight: '100%' }}>
-                    <input onChange = { e => updateText(e.target.value)} type="text"></input>
-                    <button onClick = { () => submit(isReply, parentComment, appendNewComment, parentPostId, text, hideInputChange)}>Save</button>
-                </div>
-            </div>
-        );
-    }
-    
-    function submit(isReply, parentComment, appendNewComment, parentPostId, text, hideInputChange){ 
-        console.log(text)
+function CommentInput({
+    isReply,
+    parentComment,
+    appendNewComment,
+    parentPostId,
+    hideInputChange,
+}) {
+    const [text, updateText] = useState('');
+    function submit(
+        isReplyT,
+        parentCommentT,
+        appendNewCommentT,
+        parentPostIdT,
+        textT,
+        hideInputChangeT
+    ) {
         let parentCommentId = 0;
         let nestLevel = 0;
 
-        if (parentComment){
-            parentCommentId = parentComment.id
-            nestLevel = parentComment.nest_level + 1
+        if (parentComment) {
+            parentCommentId = parentComment.id;
+            nestLevel = parentComment.nest_level + 1;
         }
-           
-        //console.log('props from commentinput submit ' + JSON.stringify(props))
-        let token = document.getElementById('csrf-token').getAttribute('content')
+        // console.log('props from commentinput submit ' + JSON.stringify(props))
+        const token = document
+            .getElementById('csrf-token')
+            .getAttribute('content');
         fetch('/api/comments/submit', {
+            // this is where you do it, check database for pfp url and also upvote the comment yourself
 
-            //this is where you do it, check database for pfp url and also upvote the comment yourself
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            
-            headers:{'X-CSRF-TOKEN': token, 'Content-Type':'application/json',},
+            headers: {
+                'X-CSRF-TOKEN': token,
+                'Content-Type': 'application/json',
+            },
             method: 'post',
-            mode: "same-origin",
-            credentials: "same-origin",
+            mode: 'same-origin',
+            credentials: 'same-origin',
             body: JSON.stringify({
                 parentPostId: parentPostId,
                 parentCommentId: parentCommentId,
                 body: text,
-                nestLevel: nestLevel
-            })
-        }).then(response => response.json(console.log(response))).then(
+                nestLevel: nestLevel,
+            }),
+        })
+            .then((response) => response.json(console.log(response)))
         data => { 
-            console.log('data from commentinput---------------------- REEEEEEEEEEEEEEEEEEEEEEEE' + JSON.stringify(data))
-            console.log()
             if (parentComment)
                 appendNewComment(data['comment'], isReply, parentComment.id)
             else
@@ -71,7 +55,39 @@ import React, { useState } from 'react';
             
             if(isReply)
                 hideInputChange();
-        })    
+        })
     }
+    return (
+        <div>
+            <div
+                style={{
+                    marginLeft: '5%',
+                    marginRight: '11%',
+                    minHeight: '100%',
+                }}
+            >
+                <input
+                    onChange={(e) => updateText(e.target.value)}
+                    type="text"
+                />
+                <button
+                    type="button"
+                    onClick={() =>
+                        submit(
+                            isReply,
+                            parentComment,
+                            appendNewComment,
+                            parentPostId,
+                            text,
+                            hideInputChange
+                        )
+                    }
+                >
+                    Save
+                </button>
+            </div>
+        </div>
+    );
+}
 
 export default CommentInput;
--- resources/js/components/Comments/PostComments.js
+++ resources/js/components/Comments/PostComments.js
@@ -1,112 +1,118 @@
-
 import React, { useState, useEffect } from 'react';
 import CommentInput from './CommentInput';
-import PostComment from './PostComment'
-//don't forget to pass the votes into the voting system component
+import PostComment from './PostComment';
+// don't forget to pass the votes into the voting system component
 
-const  PostComments = ({userId, parentPostId}) => {
-    
+const PostComments = ({ userId, parentPostId }) => {
     const [isLoading, changeLoading] = useState(true);
     const [commentsArray, changeCommentsArray] = useState([]);
-
-    useEffect(() => {
-        fetchData();
-    }, []);
-    const fetchData =() =>{
-        let url = new URL('http://localhost:80/api/comments')
-        let param = {query: parentPostId}
+    const fetchData = () => {
+        const url = new URL('http://localhost:80/api/comments');
+        const param = { query: parentPostId };
 
         url.search = new URLSearchParams(param).toString();
         fetch(url, {
-            headers:{'X-CSRF-TOKEN': document.getElementById('csrf-token').getAttribute('content'),'Content-Type':'application/json',"Access-Control-Allow-Origin" : "*", "Access-Control-Allow-Credentials" : true },
+            headers: {
+                'X-CSRF-TOKEN': document
+                    .getElementById('csrf-token')
+                    .getAttribute('content'),
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Credentials': true,
+            },
             method: 'get',
-            mode: "cors",
-            credentials: "same-origin",
-            }).then((response) => {
-                response.json().then((data) => {
-                    console.log("Data ------------------------" + JSON.stringify(data))
-                    changeLoading(false)
-                    changeCommentsArray(data['comments'])
-                });
-            })
-    }
-    function appendNewComment(commentObject, isReply, parentCommentId){
-        
-        let tempCommentsArray = [...commentsArray]; 
+            mode: 'cors',
+            credentials: 'same-origin',
+        }).then((response) => {
+            response.json().then((data) => {
+                console.log(
+                    `Data ------------------------${JSON.stringify(data)}`
+                );
+                changeLoading(false);
+                changeCommentsArray(data.comments);
+            });
+        });
+    };
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    function appendNewComment(commentObject, isReply, parentCommentId) {
+        const tempCommentsArray = [...commentsArray];
         let index;
-        if (isReply){
-            for (let i = 0; i < tempCommentsArray.length; i++) {
-                if(tempCommentsArray[i]['id'] === parentCommentId){
-                    console.log('i: ' + i)
-                  index = i + 1;
-                  console.log('index: ' + index)
+        if (isReply) {
+            for (let i = 0; i < tempCommentsArray.length; i += 1) {
+                if (tempCommentsArray[i].id === parentCommentId) {
+                    console.log(`i: ${i}`);
+                    index = i + 1;
+                    console.log(`index: ${index}`);
                 }
             }
-            tempCommentsArray.splice(index,0, commentObject)
-            console.log(tempCommentsArray)
-            changeCommentsArray(tempCommentsArray)
-        }else{
-            tempCommentsArray.unshift(commentObject)
-            console.log(tempCommentsArray)
-            changeCommentsArray(tempCommentsArray)
+            tempCommentsArray.splice(index, 0, commentObject);
+            console.log(tempCommentsArray);
+            changeCommentsArray(tempCommentsArray);
+        } else {
+            tempCommentsArray.unshift(commentObject);
+            console.log(tempCommentsArray);
+            changeCommentsArray(tempCommentsArray);
         }
     }
-    function deleteComment(id){
-    
-        //if the comment is deleted and a parrent then just update the properties to say deleted comment
-        let token = document.getElementById('csrf-token').getAttribute('content')
-        //ADD THE FETCH
+    function deleteComment(id) {
+        // if the comment is deleted and a parrent then just update the properties to say deleted comment
+        const token = document
+            .getElementById('csrf-token')
+            .getAttribute('content');
+        // ADD THE FETCH
         fetch('/api/comments/delete', {
-            headers:{'X-CSRF-TOKEN': token, 'Content-Type':'application/json',},
+            headers: {
+                'X-CSRF-TOKEN': token,
+                'Content-Type': 'application/json',
+            },
             method: 'put',
-            mode: "same-origin",
-            credentials: "same-origin",
-            body: JSON.stringify({id: id})
-        }).then(
-            data => { 
-                console.log('data from commentinput----------------------' + JSON.stringify(data))
-                /*
-                let tempCommentsArray = [...commentsArray]; 
-                let index;
-                for (let i = 0; i < tempCommentsArray.length; i++) {
-                    if(tempCommentsArray[i]['id'] === id){
-                        console.log('id: '+ id)
-                        console.log('commentId: ' + tempCommentsArray[i]['id'])
-                        console.log('i: ' + i)
-                      index = i;
-                      console.log('index: ' + index)
-                    }
-                
-                tempCommentsArray.splice(index,1)
-                console.log(tempCommentsArray)
-                changeCommentsArray(tempCommentsArray)
-                
-                } */
-            })
-    } 
-    if (isLoading) { return <div className="App">Loading...</div> }
+            mode: 'same-origin',
+            credentials: 'same-origin',
+            body: JSON.stringify({ id: id }),
+        }).then((data) => {
+            console.log(
+                `data from commentinput----------------------${JSON.stringify(
+                    data
+                )}`
+            );
+        });
+    }
+    if (isLoading) {
+        return <div className="App">Loading...</div>;
+    }
     return (
-        <div style = {{marginTop: '20px'}}>
-            <hr style={{marginBottom: '20px'}}></hr>
-            <div style = {{    marginTop: '20px', marginLeft:' 6.5%', width: '80%'}}>
-                <CommentInput style={{height: '120px'}} 
-                isReply={false}
-                parentComment={null}   
-                appendNewComment = {appendNewComment} 
-                parentPostId={parentPostId}>
-                hideInput={null}
+        <div style={{ marginTop: '20px' }}>
+            <hr style={{ marginBottom: '20px' }} />
+            <div
+                style={{ marginTop: '20px', marginLeft: ' 6.5%', width: '80%' }}
+            >
+                <CommentInput
+                    style={{ height: '120px' }}
+                    isReply={false}
+                    parentComment={null}
+                    appendNewComment={appendNewComment}
+                    parentPostId={parentPostId}
+                >
+                    hideInput={null}
                 </CommentInput>
             </div>
-            
-            <div id = "commentsholder">
-            {
-                commentsArray.map((element)=>(
-                    <PostComment key = {element.id} userId={userId} deleteComment = {deleteComment} appendNewComment = {appendNewComment} comment = {element}></PostComment>
-            ))}
+
+            <div id="commentsholder">
+                {commentsArray.map((element) => (
+                    <PostComment
+                        key={element.id}
+                        userId={userId}
+                        deleteComment={deleteComment}
+                        appendNewComment={appendNewComment}
+                        comment={element}
+                    />
+                ))}
             </div>
         </div>
     );
-}
+};
 
-   
 export default PostComments;
